c9b2724cb553a387156c726388c1ad899497707e
hosam <hosamchung998@gmail.com>
2025-05-16 10:16:41 +0800

    fix product model incorrect properties

--- src/main/java/com/example/etiqa_demo/model/Product.java
+++ src/main/java/com/example/etiqa_demo/model/Product.java
@@ -5,18 +5,24 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "customer")
+@Table(name = "product")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String firstName;
-    private String lastName;
-    private String email;
+    private String bookTitle;
+    private String bookPrice;
+    private int bookQuantity;
 }
