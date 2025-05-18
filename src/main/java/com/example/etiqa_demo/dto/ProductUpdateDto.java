b3d6b71b8a7cddc47049961f3dc970fc956d01b5
hosam <hosamchung998@gmail.com>
2025-05-18 20:02:03 +0800

    ProductUpdateDto use builder

--- src/main/java/com/example/etiqa_demo/dto/ProductUpdateDto.java
+++ src/main/java/com/example/etiqa_demo/dto/ProductUpdateDto.java
@@ -1,10 +1,12 @@
 package com.example.etiqa_demo.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class ProductUpdateDto {
     private String bookTitle;
     private String bookPrice;
